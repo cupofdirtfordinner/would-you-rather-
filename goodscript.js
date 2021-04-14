9f0c3c11b065d1f60d23b9dc6398ca91c0c25e63
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2021-04-14 13:49:48 -0500

    added some javascript I edited and "borrowed" from stack overflow.

---
+++ goodscript.js
@@ -0,0 +1,55 @@
+const readline = require('readline');
+    const fs = require('fs');
+    const FILE_PATH = 'good.txt';
+    
+    module.exports = async () =>
+    {
+        const linesCount = await getLinesCount();
+        const randomLineIndex = Math.floor(Math.random() * linesCount);
+        const content = await getLineContent(randomLineIndex);
+        return content;
+    };
+    
+    //
+    // HELPERS
+    //
+    
+    function getLineReader()
+    {
+        return readline.createInterface({
+            input: fs.createReadStream(FILE_PATH)
+        });
+    }
+    
+    async function getLinesCount()
+    {
+        return new Promise(resolve =>
+        {
+            let counter = 0;
+            getLineReader()
+            .on('line', function (line)
+            {
+                counter++;
+            })
+            .on('close', () =>
+            {
+                resolve(counter);
+            });
+        });
+    }
+    
+    async function getLineContent(index)
+    {
+        return new Promise(resolve =>
+        {
+            let counter = 0;
+            getLineReader().on('line', function (line)
+            {
+                if (counter === index)
+                {
+                    resolve(line);
+                }
+                counter++;
+            });
+        });
+    }
